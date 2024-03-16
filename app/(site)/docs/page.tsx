88708667834546dbee870d4e0dc145f2a242c8b3
ram_tanniru <ramtanniru2@gmail.com>
2024-03-16 23:22:58 +0530

    edit-5

--- app/(site)/docs/page.tsx
+++ app/(site)/docs/page.tsx
@@ -87,9 +87,9 @@ const DocsPage = () => {
     ];
 
     return (
-        <div>
+        <div className='pt-25 pb-15 mx-5 md:mx-20 md:pt-40 md:pb-30 flex flex-col '>
             <h3>These are Your Docs</h3>
-            <div className='p-3 d-flex flex-wrap gap-3 justify-content-between'>
+            <div className='p-3 flex flex-wrap gap-3 justify-between'>
                 {doctors.map((doctor, index) => (
                     <Doc key={index} doctor={doctor} />
                 ))}
